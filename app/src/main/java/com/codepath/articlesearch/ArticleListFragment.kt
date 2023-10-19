a50173d5e72147c3591d35bfefb0737b489f38aa
Christopher Mejia <58187990+chrism1001@users.noreply.github.com>
2023-10-19 16:40:28 -0400

    updating fragment list and adapter class

--- app/src/main/java/com/codepath/articlesearch/ArticleListFragment.kt
+++ app/src/main/java/com/codepath/articlesearch/ArticleListFragment.kt
@@ -2,48 +2,56 @@ package com.codepath.articlesearch
 
 import android.os.Bundle
 import android.util.Log
-import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.codepath.articlesearch.databinding.ActivityMainBinding
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
-import kotlinx.serialization.json.Json
 import okhttp3.Headers
 import org.json.JSONException
 
-fun createJson() = Json {
-    isLenient = true
-    ignoreUnknownKeys = true
-    useAlternativeNames = false
-}
-
-private const val TAG = "MainActivity/"
+private const val TAG = "ArticleListFragment"
 private const val SEARCH_API_KEY = BuildConfig.API_KEY
 private const val ARTICLE_SEARCH_URL =
     "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=${SEARCH_API_KEY}"
 
-class MainActivity : AppCompatActivity() {
+
+class ArticleListFragment : Fragment() {
+    // Add these properties
     private val articles = mutableListOf<Article>()
     private lateinit var articlesRecyclerView: RecyclerView
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var articleAdapter: ArticleAdapter
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Change this statement to store the view in a variable instead of a return statement
+        val view = inflater.inflate(R.layout.fragment_article_list, container, false)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
-
-        articlesRecyclerView = findViewById(R.id.nav_articles)
-        val articleAdapter = ArticleAdapter(this, articles)
+        // Add these configurations for the recyclerView and to configure the adapter
+        val layoutManager = LinearLayoutManager(context)
+        articlesRecyclerView = view.findViewById(R.id.article_recycler_view)
+        articlesRecyclerView.layoutManager = layoutManager
+        articlesRecyclerView.setHasFixedSize(true)
+        articleAdapter = ArticleAdapter(view.context, articles)
         articlesRecyclerView.adapter = articleAdapter
-        articlesRecyclerView.layoutManager = LinearLayoutManager(this).also {
-            val dividerItemDecoration = DividerItemDecoration(this, it.orientation)
-            articlesRecyclerView.addItemDecoration(dividerItemDecoration)
-        }
 
+        // Update the return statement to return the inflated view from above
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Call the new method within onViewCreated
+        fetchArticles()
+    }
+
+
+    private fun fetchArticles() {
         val client = AsyncHttpClient()
         client.get(ARTICLE_SEARCH_URL, object : JsonHttpResponseHandler() {
             override fun onFailure(
@@ -72,6 +80,12 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
+    }
 
+
+    companion object {
+        fun newInstance(): ArticleListFragment {
+            return ArticleListFragment()
+        }
     }
 }
